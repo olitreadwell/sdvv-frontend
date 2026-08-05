415e0f9fdcb1a6794b55d4bae6f1d700460b1ed0
Robert <robertgz@users.noreply.github.com>
2026-08-04 21:22:29 -0700

    fix(lib-ui-charts): add ContributionsByCodeStackedBarComponent export to public-api, fix import of component in ContributionsByCodeGQLComponent, remove ContributionsByCodeStackedBarModule

--- projects/lib-ui-charts/src/lib/contributions-by-code-stacked-bar/contributions-by-code-stacked-bar.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ContributionsByCodeStackedBarComponent } from './contributions-by-code-stacked-bar.component';
-
-@NgModule({
-  declarations: [],
-  imports: [ContributionsByCodeStackedBarComponent],
-  exports: [ContributionsByCodeStackedBarComponent],
-})
-export class ContributionsByCodeStackedBarModule {}
